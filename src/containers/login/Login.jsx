c8fb707254db0cc353b6083390fdf566d5da1096
Huy Ngo <hngo@matrixformedia.onmicrosoft.com>
2023-01-13 07:56:22 -0500

    demo

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <button className="text-red-500">Login</button>
+      <button>Logout</button>
+    </>
+  );
 };
 
 export default Login;
