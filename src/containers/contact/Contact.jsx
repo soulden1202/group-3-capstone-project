30d1637bf1480ee13edc495ce58398898432660b
Yash Patel <78617062+Ypatel02@users.noreply.github.com>
2023-02-10 23:27:14 -0500

    Add files via upload
    Made a contact feature to see how contact page should look once it is done.

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import {grey} from "@mui/material/colors";
 
 const ContactUs = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (event) => {
@@ -15,43 +16,49 @@ const ContactUs = () => {
   };
 
   return (
-    <div>
-      {submitted ? (
-        <div>Thank you for contacting us!</div>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              size={30}
-              type="text"
-              id="name"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              size={30}
-              type="email"
-              id="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-            />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      )}
-    </div>
+
+
+      <div>
+
+        {submitted ? (
+            <div>Thank you for contacting us!</div>
+        ) : (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    size={30}
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+              </div>
+              <div>
+
+                <label htmlFor="email">Email:</label>
+                <input
+                    size={30}
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message:</label>
+                <textarea
+
+                    id="message"
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+        )}
+      </div>
+
   );
 };
 
