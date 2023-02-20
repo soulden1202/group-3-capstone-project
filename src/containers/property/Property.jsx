c6b7598c9b1b5abe7b2740603832c41ac3b4787e
Huy Ngo <hngo@matrixformedia.onmicrosoft.com>
2023-02-20 00:27:05 -0500

    this is a basic layout for property page

--- src/containers/property/Property.jsx
+++ src/containers/property/Property.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { PropertyCard } from "./PropertyCard";
 
 const Property = () => {
-  return <div>Property</div>;
+  const testobj = [
+    { address: "abc", id: 1 },
+    { address: "abc", id: 2 },
+    { address: "abc", id: 3 },
+    { address: "abc", id: 4 },
+  ];
+
+  return (
+    <div className="flex flex-col fixed w-full h-full ">
+      <div>Search Bar</div>
+      <div className="flex md:flex-row flex-col w-full h-full ">
+        <div className="flex w-full md:w-[40%] h-full flex-col">
+          {testobj.map((value, key) => (
+            <PropertyCard propId={value.id} />
+          ))}
+        </div>
+        <div className="w-[0%] md:w-[60%] md:flex hidden h-full justify-center items-center">
+          Map display
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Property;
---
+++ src/containers/property/PropertyDetailModal.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+export const PropertyDetailModal = ({ propId }) => {
+  const [open, setopen] = useState(false);
+
+  return (
+    <>
+      <button
+        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+        onClick={() => setopen(true)}
+      >
+        More Detail
+      </button>
+      {open === true && (
+        <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto inset-0 h-ful">
+          <div className="flex relative w-full h-full ">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-800">
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  Terms of Service {propId}
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  onClick={() => setopen(false)}
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  With less than a month to go before the European Union enacts
+                  new consumer privacy laws for its citizens, companies around
+                  the world are updating their terms of service agreements to
+                  comply.
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  The European Union’s General Data Protection Regulation
+                  (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
+                  common set of data rights in the European Union. It requires
+                  organizations to notify users as soon as possible of high-risk
+                  data breaches that could personally affect them.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
