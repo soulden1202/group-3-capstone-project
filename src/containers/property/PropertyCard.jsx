6e1b2f0d22d25ae6c2728252e4ba401cd5465d8a
Huy Ngo <hngo@matrixformedia.onmicrosoft.com>
2023-02-24 22:19:25 -0500

    this add search function and display map on property page

--- src/containers/property/PropertyCard.jsx
+++ src/containers/property/PropertyCard.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import img from "../../../src/assets/img1.jpg";
 import { PropertyDetailModal } from "./PropertyDetailModal";
 
-export const PropertyCard = ({ propId }) =>
+export const PropertyCard = ({ homeData, onCardSelected }) =>
   //   propertyId,
   //   address,
   //   numOfBedroom,
@@ -12,22 +11,35 @@ export const PropertyCard = ({ propId }) =>
 
   {
     return (
-      <div className="flex w-full h-[20%] border-[1px] border-blue-400 mt-1 ml-1 rounded-md flex-row hover:border-blue-600 hover:border-2">
+      <div
+        className="flex w-full h-auto border-[1px] border-blue-400 mt-1 ml-1 rounded-md flex-row hover:border-blue-600 hover:border-2"
+        onClick={() =>
+          onCardSelected(
+            homeData.address,
+            homeData.city,
+            homeData.state,
+            homeData.zipCode
+          )
+        }
+      >
         <div className="flex w-[30%] h-full mr-1 object-cover items-center justify-center bg-gray-100">
-          <img src={img} alt="Home" />
+          <img src={homeData.coverImage} alt="Home" />
         </div>
         <div className="flex w-[70%] flex-row">
           <div className="flex w-[80%] flex-col ">
-            <div>123 King raoad, PA,14444</div>
-            <div>Bedroom(s): 2</div>
-            <div>Bathroom(s): 1</div>
-            <div>Year Built: 1990</div>
-            <div>1920ft</div>
+            <div>
+              {homeData.address}, {homeData.city},{" "}
+              {homeData.state.toUpperCase()}, {homeData.zipCode}
+            </div>
+            <div>Bedroom(s): {homeData.numberOfBedroom}</div>
+            <div>Bathroom(s): {homeData.numberOfBathroom}</div>
+            <div>Year Built: {homeData.yearBuilt}</div>
+            <div>{homeData.acreage}ft</div>
             <div className="md:hidden flex text-blue-700">Display on Map</div>
           </div>
           <div className="flex w-[20%] flex-col mr-3">
             <div className="flex w-full h-full items-center justify-end">
-              <PropertyDetailModal propId={propId}></PropertyDetailModal>
+              <PropertyDetailModal homeData={homeData}></PropertyDetailModal>
             </div>
           </div>
         </div>
