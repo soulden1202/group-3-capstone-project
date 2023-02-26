8eee1063b5ff634067dfe595164b0f32a80be50d
shreyas <hardshreyas@gmail.com>
2023-02-26 18:42:06 -0500

    tailwind used for login and signup

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./login.css";
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -14,52 +13,74 @@ const Login = () => {
     };
 
     const handleLogin = () => {
-        fetch('https://studentrentapi20230210185810.azurewebsites.net/api/Auth/login', {
-            method: 'POST',
+        fetch("https://studentrentapi20230210185810.azurewebsites.net/api/Auth/login", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 email: email,
-                password: password
-            })
+                password: password,
+            }),
         })
-            .then(response => {
+            .then((response) => {
                 if (!response.ok) {
-                    throw new Error('Login failed');
+                    throw new Error("Login failed");
                 }
-                return response.json();
+                return response.text();
             })
-            .then(data => {
+            .then((data) => {
                 console.log(data);
                 // handle successful login here
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error(error);
                 // handle login error here
             });
     };
-
 
     const handleForgotPassword = () => {
         console.log("Forgot password clicked");
     };
 
     return (
-        <div className="login-container">
-            <div className="form-container">
-                <label>
-                    Email:
-                    <input type="text" value={email} onChange={handleEmailChange} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={handlePasswordChange} />
-                </label>
-                <br />
-                <button className="login-button" onClick={handleLogin}>Log In</button>
-                <button className="forgot-password-button" onClick={handleForgotPassword}>Forgot Password</button>
+        <div className="flex h-screen bg-gray-100">
+            <div className="m-auto w-96">
+                <div className="bg-white p-8 rounded-lg shadow-lg">
+                    <h2 className="text-3xl font-semibold mb-4">Log In</h2>
+                    <div className="space-y-4">
+                        <label className="block">
+                            <span className="text-gray-700">Email:</span>
+                            <input
+                                type="text"
+                                value={email}
+                                onChange={handleEmailChange}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                            />
+                        </label>
+                        <label className="block">
+                            <span className="text-gray-700">Password:</span>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={handlePasswordChange}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                            />
+                        </label>
+                        <button
+                            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                            onClick={handleLogin}
+                        >
+                            Log In
+                        </button>
+                        <button
+                            className="text-gray-600 hover:text-gray-700"
+                            onClick={handleForgotPassword}
+                        >
+                            Forgot Password
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/containers/signup/Signup.jsx
+++ src/containers/signup/Signup.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./signup.css";
 
 const Signup = () => {
     const [firstName, setFirstName] = useState("");
@@ -29,26 +28,26 @@ const Signup = () => {
     };
 
     const handleSignup = () => {
-        fetch('https://studentrentapi20230210185810.azurewebsites.net/api/Auth/register', {
-            method: 'POST',
+        fetch("https://studentrentapi20230210185810.azurewebsites.net/api/Auth/register", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 FirstName: firstName,
                 LastName: lastName,
                 email: email,
                 password: password,
-                AccountType: accountType
-            })
+                AccountType: accountType,
+            }),
         })
-            .then(response => {
+            .then((response) => {
                 if (!response.ok) {
-                    throw new Error('Signup failed');
+                    throw new Error("Signup failed");
                 }
                 return response.text();
             })
-            .then(data => {
+            .then((data) => {
                 console.log(data);
                 if (data.includes("Account created and added to Firestore")) {
                     // handle successful signup here
@@ -57,47 +56,110 @@ const Signup = () => {
                     console.log("Signup failed");
                 }
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error(error);
                 // handle signup error here
             });
     };
 
-
-
     return (
-        <div className="signup-container">
-            <div className="form-container">
-                <label>
-                    First Name:
-                    <input type="text" value={firstName} onChange={handleFirstNameChange} />
-                </label>
-                <br />
-                <label>
-                    Last Name:
-                    <input type="text" value={lastName} onChange={handleLastNameChange} />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input type="text" value={email} onChange={handleEmailChange} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={handlePasswordChange} />
-                </label>
-                <br />
-                <label>
-                    Account Type:
-                    <select value={accountType} onChange={handleAccountTypeChange}>
-                        <option value="">Select Account Type</option>
-                        <option value="Renter">Renter</option>
-                        <option value="Property Owner">Property Owner</option>
-                    </select>
-                </label>
-                <br />
-                <button className="signup-button" onClick={handleSignup}>Create Account</button>
+        <div className="flex justify-center mt-20">
+            <div className="w-full max-w-sm">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="firstName"
+                        >
+                            First Name:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="firstName"
+                            type="text"
+                            placeholder="First Name"
+                            value={firstName}
+                            onChange={handleFirstNameChange}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="lastName"
+                        >
+                            Last Name:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="lastName"
+                            type="text"
+                            placeholder="Last Name"
+                            value={lastName}
+                            onChange={handleLastNameChange}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="email"
+                        >
+                            Email:
+                        </label>
+                        <div className="mb-4">
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="email"
+                                type="email"
+                                placeholder="Email Address"
+                                value={email}
+                                onChange={handleEmailChange}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 text-sm font-bold mb-2"
+                                htmlFor="password"
+                            >
+                                Password:
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="password"
+                                type="password"
+                                placeholder="********"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 text-sm font-bold mb-2"
+                                htmlFor="accountType"
+                            >
+                                Account Type:
+                            </label>
+                            <select
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="accountType"
+                                value={accountType}
+                                onChange={handleAccountTypeChange}
+                            >
+                                <option value="">Select an account type</option>
+                                <option value="Renter">Renter</option>
+                                <option value="Property Owner">Property Owner</option>
+                            </select>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="button"
+                                onClick={handleSignup}
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
