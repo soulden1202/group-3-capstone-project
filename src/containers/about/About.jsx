386694aeff945370e25b2a1f0b3125776f1cc445
Yash Patel <78617062+Ypatel02@users.noreply.github.com>
2023-03-13 17:32:22 -0400

    ADD change to homepage

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -20,9 +20,9 @@ const About = () => {
       <div className="team-box">
         <h1 style={titleStyles}> About Us </h1>
 
-        <h2 style={{ marginTop: "50px" }}>Our Mission is to help students</h2>
-        <h2>to find best homes</h2>
-        <h2>or apartments in their badge. </h2>
+        <h2 style={{ marginTop: "50px" }}>Our team of experienced professionals is dedicated to helping students to find their dream place to stay.</h2>
+        <h2>Whether you're buying, selling, or renting, we're here to guide you every step of the way.</h2>
+        <h2>Let's work together to make your real estate journey a success. </h2>
 
         <div className="team-container">
           <h2 style={teamStyles}>Meet Our Team</h2>
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,95 +1,40 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import "./Home.css";
 
+
 const Home = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(countries);
-    setSubmitted(true);
-
-    console.log(countries.filter((entry) => entry.name === searchQuery));
-  };
   useEffect(() => {
     document.title = "Home - Livin it";
   }, []);
 
-  //temp data below to test out the search bar
-  const countries = [
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent: "Asia" },
-  ];
+    const titleStyles = {
+        fontSize: "40px",
+    };
+
+    const parStyle = {
+        fontSize: "20px",
+    };
+
 
   return (
     <div className="fixed w-full h-full ">
       <div className=" bg-cover bg-homepageBg h-full w-full ">
-        <div className="flex h-screen justify-center items-center">
-          <div className="text-gray-800 text-center border border-gray-500 rounded-lg px-4 py-2 m-2">
-            {submitted ? (
-              <div>Your Searching result!</div>
-            ) : (
-              <form onSubmit={handleSubmit}>
-                <input
-                  value={searchQuery}
-                  onInput={(e) => setSearchQuery(e.target.value)}
-                  type="text"
-                  id="header-search"
-                  placeholder="Enter an address, neighborhood, city, or ZIP code"
-                  name="s"
-                />
-                <button
-                  type="submit"
-                  className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  <svg
-                    aria-hidden="true"
-                    className="w-5 h-5 mr-2 -ml-1"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
-                  Search
-                </button>
+          <div className="information flex justify-center items-center">
+              <h1 style={titleStyles}>Let's find your dream house</h1>
+          </div>
+          <div className="par flex justify-center items-center">
+              <p style={parStyle}>At Livin it we don't just show you houses we make your dream come true</p>
+          </div>
+          <div className="p-3 flex justify-center items-center">
+              <form action={"/property"}>
+                  <button className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                  focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      type="submit"><strong>Find your home today</strong></button>
               </form>
-            )}
           </div>
         </div>
-      </div>{" "}
     </div>
-  );
+        );
 };
-
 export default Home;
