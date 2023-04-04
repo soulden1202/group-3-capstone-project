568db5a7f57f3809846da59eacbf12a11e9426b3
shreyas <hardshreyas@gmail.com>
2023-04-03 23:19:37 -0400

    made watchlist , frontend not working

--- src/containers/property/PropPortfolio.jsx
+++ src/containers/property/PropPortfolio.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Geocode from "react-geocode";
 import { useParams } from "react-router-dom";
 import { PropertyDetailModal } from "./PropertyDetailModal";
 import { useSelector } from "react-redux";
@@ -11,10 +10,10 @@ const PropPortfolio = () => {
     const user = useSelector((state) => state.user);
     useEffect(() => {
         if (!user.id) {
-            navigate('/login');
+            navigate("/login");
         }
     }, [user.id, navigate]);
-    Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAP_API_KEY);
+
     const searchUrl =
         "https://studentrentapi20230322222647.azurewebsites.net/api/Property/SearchById";
 
@@ -44,57 +43,49 @@ const PropPortfolio = () => {
             },
         }).then((res) => {
             sethomeData(res.data);
-            setLatandlng(res.data);
         });
     };
 
     const onCardSelected = (address, city, state, zipCode) => {
-        var searhAdress = `${address}, ${city}, ${state.toUpperCase()}, ${zipCode}`;
-        Geocode.fromAddress(searhAdress).then(
-            (response) => {
-                //const { lat, lng } = response.results[0].geometry.location;
-                //const center = { lat: lat, lng: lng };
-                //const position = { lat: lat, lng: lng };
-                // console.log(center, position);
-            },
-            (error) => {
-                console.error(error);
-            }
-        );
+        console.log(`Address: ${address}, City: ${city}, State: ${state}, Zip Code: ${zipCode}`);
     };
 
-    const setLatandlng = (data) => {
-        data.forEach(async (home) => {
-            var searhAdress = `${home.address}, ${home.city
-                }, ${home.state.toUpperCase()}, ${home.zipCode}`;
-
-            await Geocode.fromAddress(searhAdress).then(
-                (response) => {
-                    
-                },
-
-                (error) => {
-                    console.error(error);
-                }
-            );
-        });
-    };
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
             {homeData.map((value, key) => (
-                <div key={key} className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                    <div className="h-56 w-full bg-cover" style={{ backgroundImage: `url(${value.mainImage})` }} onClick={() => onCardSelected(value.address, value.city, value.state, value.zipCode)}>
+                <div
+                    key={key}
+                    className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
+                >
+                    <div
+                        className="h-57 w-full bg-cover relative"
+                        style={{ backgroundImage: "none" }}
+                        onClick={() =>
+                            onCardSelected(
+                                value.address,
+                                value.city,
+                                value.state,
+                                value.zipCode
+                            )
+                        }
+                    >
+                        <img
+                            src={value.coverImage}
+                            alt={value.address}
+                            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                        />
                         <div className="flex justify-end items-end h-full w-full bg-gray-900 bg-opacity-50"></div>
                     </div>
                     <div className="px-5 py-3">
                         <h3 className="text-gray-700 uppercase">{value.address}</h3>
                         <span className="text-gray-500 mt-2">${value.price}</span>
                     </div>
-                    <PropertyDetailModal homeData={value} withId={withId} />
+                    <div className="absolute bottom-0 left-0 right-0 px-5 pb-3">
+                        <PropertyDetailModal homeData={value} withId={withId} />
+                    </div>
                 </div>
             ))}
         </div>
     );
-
 };
 export default PropPortfolio;
--- src/containers/user/WatchingList.jsx
+++ src/containers/user/WatchingList.jsx
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { PropertyDetailModal } from "../property/PropertyDetailModal";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const WatchingList = () => {
-  return (
-    <div className="z-0 fixed w-full h-full">
-      <div className="flex items-center justify-center">wishlist</div>
-    </div>
-  );
+const Watchinglist = () => {
+    let navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const watchlist = useSelector((state) => state.watchlist);
+
+    useEffect(() => {
+        if (!user.id) {
+            navigate("/login");
+        }
+    }, [user.id, navigate]);
+
+    const searchUrl =
+        "https://studentrentapi20230322222647.azurewebsites.net/api/Property/SearchById";
+
+    const [homeData, sethomeData] = useState([]);
+
+    const search = async () => {
+        let data = [];
+        for (let i = 0; i < watchlist.length; i++) {
+            await axios({
+                method: "post",
+                url: searchUrl,
+                headers: {},
+                data: {
+                    propertyId: watchlist[i],
+                },
+            }).then((res) => {
+                data.push(res.data[0]);
+            });
+        }
+        sethomeData(data);
+    };
+
+    useEffect(() => {
+        document.title = "Watchlist - Livin it";
+        search();
+    }, [search]);
+
+    const onCardSelected = (address, city, state, zipCode) => {
+        console.log(
+            `Address: ${address}, City: ${city}, State: ${state}, Zip Code: ${zipCode}`
+        );
+    };
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
+            {homeData.map((value, key) => (
+                <div
+                    key={key}
+                    className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
+                >
+                    <div
+                        className="h-57 w-full bg-cover relative"
+                        style={{ backgroundImage: "none" }}
+                        onClick={() =>
+                            onCardSelected(
+                                value.address,
+                                value.city,
+                                value.state,
+                                value.zipCode
+                            )
+                        }
+                    >
+                        <img
+                            src={value.coverImage}
+                            alt={value.address}
+                            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                        />
+                        <div className="flex justify-end items-end h-full w-full bg-gray-900 bg-opacity-50"></div>
+                    </div>
+                    <div className="px-5 py-3">
+                        <h3 className="text-gray-700 uppercase">{value.address}</h3>
+                        <span className="text-gray-500 mt-2">${value.price}</span>
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 px-5 pb-3">
+                        <PropertyDetailModal homeData={value} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
-export default WatchingList;
+export default Watchinglist;
